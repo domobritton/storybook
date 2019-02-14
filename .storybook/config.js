f3141b31e2c81efc788e2eda207d9624ab067a6c
domobritton <domonicbritton@gmail.com>
2019-02-13 17:35:37 -0800

    Basic task ui component

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,18 @@
+// import { configure } from '@storybook/react';
+
+// function loadStories() {
+//   require('../src/stories');
+// }
+
+// configure(loadStories, module);
+
 import { configure } from '@storybook/react';
+import '../src/index.css';
+
+const req = require.context('../src', true, /.stories.js$/);
 
 function loadStories() {
-  require('../src/stories');
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
